777516d96862d50912a768dde547080f0679c441
Ender <student.us.ender90@gmail.com>
2019-04-09 20:49:51 +0200

    Dodanie Data Pickera do GUI oraz ustawienie go w kontrolerze na lokalną datę

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -6,11 +6,13 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import sample.entity.Filmy;
 import sample.entity.Seanse;
 import sample.jdbc.DatabaseConnector;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -23,11 +25,21 @@ public class Controller implements Initializable {
     @FXML
     private ComboBox<Seanse> comboBoxSeans;
 
+    @FXML
+    private DatePicker calendarPicker;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ustawDate();
         wczytajFilmy();
         wczytajSeans();
+    }
+
+    @FXML
+    private void ustawDate(){
+        calendarPicker.setValue(LocalDate.now());
+        LocalDate data = calendarPicker.getValue();
     }
 
     @FXML
