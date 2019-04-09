42f82fe60082f3a9b9d525ea8b2746188744d551
Ender <student.us.ender90@gmail.com>
2019-04-09 20:48:28 +0200

    Usunięcie zbędnego zakomentowanego połączenia z bazą danych

--- src/sample/jdbc/DatabaseConnector.java
+++ src/sample/jdbc/DatabaseConnector.java
@@ -172,8 +172,6 @@ public class DatabaseConnector{
     public DatabaseConnector(){
         System.out.println(getClass().getResource("/resources/static/production.db"));
         try {
-            //this.connection = DriverManager.getConnection("jdbc:sqlite::resource:" + getClass().getResource("/resources/static/production.db"));
-
             this.connection = DriverManager.getConnection("jdbc:sqlite:src/sample/resources/static/" + DB_NAME);
         }catch (SQLException e){
             System.out.println(e.getMessage());
